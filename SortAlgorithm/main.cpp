a80b4e6a253d8b12c5eb30cdeb56b45ba1232358
Wang Chengwen <user@usertekiiMac.local>
2013-06-03 09:55:10 +0800

    Initial Commit

---
+++ SortAlgorithm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SortAlgorithm
+//
+//  Created by Wang Chengwen on 13-6-3.
+//  Copyright (c) 2013年 user. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
